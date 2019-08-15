b54344b850d03be3898e52674168283b106b0cf2
ColeyG <cjgeerts@gmail.com>
2019-08-15 12:16:30 -0400

    Add image sources

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import prawRequest
 
-prawRequest.PrawRequest().req("art", 10, ["images"])
+prawRequest.PrawRequest().req("art", 1000, ["images"])
+prawRequest.PrawRequest().req("pics", 1000, ["images"])
+prawRequest.PrawRequest().req("pic", 1000, ["images"])
+prawRequest.PrawRequest().req("pictures", 1000, ["images"])
+prawRequest.PrawRequest().req("images", 1000, ["images"])
 # schedule.every(1).minutes.do(prawRequest.PrawRequest().req("dog", 1))
